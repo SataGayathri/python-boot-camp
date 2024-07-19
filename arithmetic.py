df1fe9c043658d054e6843b6351ad8c7e69786cc
SataGayathri <128565799+SataGayathri@users.noreply.github.com>
2024-07-19 15:32:51 +0530

    Day-1

---
+++ arithmetic.py
@@ -0,0 +1,69 @@
+#a=int(input())
+#b=int(input())
+#print(a+b)
+#print(a-b)
+#print(a/b)
+#print(a*b)
+#print(a%b)
+#print(a**b)
+#prrint(a//b)
+
+#logical operator --- and,or
+#age=int(input())
+#if(age>=18 and age<24):
+   # print("only two wheeler")
+#elif(age>24 and age<45):
+   # print(" two and four wheeler")
+#else:
+    #print("all")   
+
+
+#apple=int(input())
+#banana=int(input())
+#orange=int(input())
+#apple=apple*15
+#banana=banana*4
+##orange=orange*5
+#cost=apple+banana+orange
+#print(cost)
+#if(cost<=700):
+   # print("x is not cheasted")
+#else:
+    #print("x is cheasted")    
+   
+
+
+
+#a=int(input())
+#if(a%2==0):
+   #if(a>=0):
+     # print("even positive number")
+   #else:
+      #print("even negative number")
+#else:  
+   # if(a>=0):
+       #print("odd positive number")
+   #else:
+       #print("odd negative number")
+  
+
+#hegiht=int(input())
+#weight=int(input())
+#bodyfat=int(input())  
+#if(height>=140 and weight%2==0)
+
+#n=int(input())
+#sum=0
+#temp=n
+#hile temp>0:
+   # sum+=digit*digit*digit
+   # temp=temp//10
+#if sum==n:
+    #print("it is an armstrong number")
+##else:
+    #print("it is not a armstrong number")
+##
+
+
+
+
---
+++ datatypes.py
@@ -0,0 +1,28 @@
+#int
+#float
+#string
+#boolean
+#sets
+#lists
+#dictionary
+#print("Hello World")
+#var1="gayathri"
+#print("good morning",var1)
+#Name=input("enter your Name")
+#print("hello",Name)
+#Name="gayathri"
+#age="20"
+#print(f"hello{Name}your are{age}years")
+#name=input()
+#s1=int(input())
+#s2=int(input())
+#s3=int(input())
+#s4=int(input())
+#s5=int(input())
+#print(f"your marks are{s1+s2+s3+s4+s5}, and avg is{(s1+s2+s3+s4+s5)/5}")
+#name=(input())
+#age=int(input())
+#if(age>18):
+   #print("your are eligible for voting")
+#else:
+   #print("your not eligible for voting")
